3547604ff3f19ffa83903894e7f046bd631a2c6f
taufiqhi220 <taufiqhi220@gmail.com>
2026-07-22 16:29:01 +0800

    chore: sembunyikan db_config.php dari git dan buat db_config.example.php

--- db_config.example.php
+++ db_config.example.php
@@ -1,8 +1,9 @@
 <?php
+// Template Konfigurasi Database (Salin ke db_config.php di server)
 $host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "db_sindesa";
+$user = "DB_USER_HERE";
+$pass = "DB_PASS_HERE";
+$dbname = "DB_NAME_HERE";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
@@ -10,4 +11,3 @@ if (!$conn) {
     header('Content-Type: application/json');
     die(json_encode(["success" => false, "message" => "Koneksi database gagal: " . mysqli_connect_error()]));
 }
-// JANGAN PAKAI TANDA PENUTUP PHP DI SINI AGAR TIDAK ADA SPASI KOSONG
